123b648078611e9f504cdb48b993d92dbed0e24b
Alexander Tsai <alext1@cs.uchicago.edu>
2019-12-29 03:48:10 -0600

    update error page

--- src/Components/error/index.js
+++ src/Components/error/index.js
@@ -7,8 +7,8 @@ const ErrorPage = () => {
         <div className="container">
             <div className="error_wrapper">
                 <h1>Error 404</h1>
-                <p>Opps! Page not Found :(</p>
-                <p>You have no access to the page or it does not exist.</p>
+                <p>Oops! Page not Found :(</p>
+                <p>You don't have access to the webpage or it does not exist.</p>
                 <Link to='/'>
                     <Button color='primary' variant="contained">Back to Home</Button>
                 </Link>
